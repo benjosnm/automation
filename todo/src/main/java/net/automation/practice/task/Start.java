63eb6fc59b6622de470c326e2ca1a2be71ea964f
benjosnm <benjo.snm@gmail.com>
2019-03-23 13:05:16 -0400

    more code

--- todo/src/main/java/net/automation/practice/task/Start.java
+++ todo/src/main/java/net/automation/practice/task/Start.java
@@ -5,6 +5,7 @@ import net.automation.practice.resources.ApplicationHomePage;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.conditions.Check;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -35,7 +36,8 @@ public class Start implements Task {
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(
                 Open.browserOn().the(applicationHomePage),
-                Refresh.theBrowserSession()
+                Refresh.theBrowserSession(),
+                Check.whether(items.isEmpty()).otherwise()
         );
     }
 }
